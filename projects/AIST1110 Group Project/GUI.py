8dde1edab6c3bf2f856827b0bdb1a9a6b4f473be
Brian <computer6ai@gmail.com>
2025-03-20 16:26:00 +0800

    Added colors as global and started rect instead of surf

--- projects/AIST1110 Group Project/GUI.py
+++ projects/AIST1110 Group Project/GUI.py
@@ -1,23 +1,29 @@
 import pygame
 from sys import exit
 
+# Some aesthetic colors
+Blue = (55, 107, 137)
+Red = (128, 25, 34)
+Yellow = (236, 178, 67)
+Light_Gray = (178, 181, 190)
+Dark_Gray = (67, 70, 81)
+
 pygame.init()
-screen_info = pygame.display.Info()
-screen_width = screen_info.current_w
-screen_height = screen_info.current_h
 screen = pygame.display.set_mode((1280, 720))
 # pygame.RESIZABLE allows window toggling but requires alignment adjustment
 # Decide whether to use pygame.RESIZABLE
 
 pygame.display.set_caption("Guessing Their Answers")
 clock = pygame.time.Clock()
+
+#TODO: Look for resources: background.png, characters.png, ...
 title_font = pygame.font.Font(r"Resources\font\Bree_Serif\BreeSerif-Regular.ttf", 40)
+title_surf = title_font.render('Guessing Their Answers', True, Light_Gray, Red)
+title_rect = title_surf.get_rect(midbottom = (640, 200))
 
-#TODO: background.png, characters, ...
-title_surf = title_font.render('Guessing Their Answerss', False, 'Grey')
 
 
-while True: 
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -29,7 +35,7 @@ while True:
     #e.g. question text field, answer text field, ...
     #OR, draw the required field using pygame.draw
     
-    screen.blit(title_surf, (300, 300))
+    screen.blit(title_surf, title_rect)
 
     pygame.display.update()
     clock.tick(60)
