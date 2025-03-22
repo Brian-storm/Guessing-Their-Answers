7423ef4fd46c98e1873b10ac7dff897a15260d87
Brian <computer6ai@gmail.com>
2025-03-22 23:34:43 +0800

    Optimized game loop and button logic; Added button animation when clicked

--- projects/AIST1110 Group Project/Sprites.py
+++ projects/AIST1110 Group Project/Sprites.py
@@ -1,19 +1,19 @@
 """
 Screen size: 1280 x 720
 This file records classes of sprites:
--Button (GUI)
--Characters
+- Button
+- Button_Group
+- Q_and_A (upcoming)
 """
 
 import pygame
-import time
-from Resources.colors_and_fonts import BREE_SERIF, LIGHT_GRAY, RED, WHITE, YELLOW
+from Resources.colors_and_fonts import BREE_SERIF, LIGHT_GRAY, RED, YELLOW
 
 
 class Button(pygame.sprite.Sprite):
-    """Button to be clicked by mouse, or tapped by keyboard"""
+    """Button to be interacted by mouse"""
 
-    def __init__(self, text: bool | str, button_color: str | int = RED,
+    def __init__(self, text: bool | str = None, button_color: str | int = LIGHT_GRAY,
                  button_size: tuple[int, int] = (220, 100),
                  *, center_pos: tuple[int, int]):
         super().__init__()
@@ -22,40 +22,57 @@ class Button(pygame.sprite.Sprite):
         self.button_color = button_color
         self.button_size = (width, height)
         self.center = center_pos
-        self.hovered = False
 
-        self.surf = pygame.Surface(self.button_size, pygame.SRCALPHA)
-        self.surf.fill(self.button_color)
+        # self.norm is the original appearance of the buttons
+        self.norm = pygame.Surface(self.button_size, pygame.SRCALPHA)
+        self.norm.fill(self.button_color)
+        self.rectA = self.norm.get_rect(center=self.center)
 
-        # Creating text surface and blit to button
+        # for is_clicked
+        self.is_clicked = False
+        self.countdown = 0
+        self.clicked = self.norm.copy()
+
+        # Creating text surface and blit to self.norm
         if self.text is not None:
-            self.text_font = pygame.font.Font(BREE_SERIF, 40)
+            self.text_font = pygame.font.Font(BREE_SERIF, 45)
             self.text_surf = self.text_font.render(self.text, True, RED, button_color)
+            self.text_surf_clicked = self.text_font.render(self.text, True, YELLOW, button_color)
             self.text_rect = self.text_surf.get_rect(center=self.center)
 
-            self.rect = self.surf.get_rect(center=self.center)
-
-            text_L, text_R = (self.center[0] - self.rect.left, self.center[1]-self.rect.top)
+            text_L, text_R = (self.center[0] - self.rectA.left, self.center[1]-self.rectA.top)
             text_center = (text_L - self.text_rect.width//2, text_R - self.text_rect.height//2)
-            self.surf.blit(self.text_surf, text_center)
+
+            self.norm.blit(self.text_surf, text_center)
+            self.clicked.blit(self.text_surf_clicked, text_center)
 
         # TODO: maybe a button without texts but with IMG
 
-        self.hover_surf = pygame.transform.rotozoom(self.surf, 6, 1.2)
-        self.hover_rect = self.hover_surf.get_rect(center=self.center)
+        # Creating hovered appearances
+        self.hovered = pygame.transform.rotozoom(self.norm, 6, 1.2)
+        self.clicked = pygame.transform.rotozoom(self.clicked, 6, 1.2)
+        self.rectB = self.hovered.get_rect(center=self.center)
 
-        self.display_surf = self.surf.copy()
-        self.display_rect = self.rect.copy()
+        # self.image and self.rect are for display (by Group.draw())
+        # AND CANNOT CHANGE THE NAMES (due to inheritance)
+        self.image = self.norm.copy()
+        self.rect = self.rectA
 
-    def is_hovered(self, mousePos):
-        if self.rect.collidepoint(mousePos):
-            self.display_surf = self.hover_surf.copy()
-            self.display_rect = self.hover_rect.copy()
+    def check_hovered(self, mousePos):
+        if self.rectA.collidepoint(mousePos):
+            self.image = self.hovered.copy()
+            self.rect = self.rectB
+        else:
+            self.image = self.norm.copy()
+            self.rect = self.rectA
 
-    def is_clicked(self, mousePos):
-        if self.rect.collidepoint(mousePos):
-            self.display_surf.fill(WHITE)
-            pygame.time.delay(50)
+    def check_clicked(self, mousePos):
+        if self.rectA.collidepoint(mousePos):
+            self.image = self.clicked.copy()
+            self.rect = self.rectB
+        else:
+            self.check_hovered(mousePos)
+
         # The white fill covers the entire rect,
         # which is bad. Optimization needed here
         # also, for transisiton scene (not yet made), 
@@ -66,18 +83,31 @@ class Button_Group(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
 
-    def update(self, mousePos, event_type):
+    def update(self, mousePos, clicked):
         for button in self.sprites():
-            if event_type == pygame.MOUSEMOTION:
-                button.is_hovered(mousePos)
-            if event_type == pygame.MOUSEBUTTONDOWN:
-                button.is_clicked(mousePos)
+            if clicked:
+                button.is_clicked = True
+                button.countdown = pygame.time.get_ticks() + 100
+                button.check_clicked(mousePos)
+            elif pygame.time.get_ticks() >= button.countdown:
+                button.is_clicked = False
+                button.countdown = 0
+                button.check_hovered(mousePos)
 
-    def return_to_normal(self):
-        for button in self.sprites():
-            button.display_surf = button.surf.copy()
-            button.display_rect = button.rect.copy()
+    # def set_to_normal(self):
+    #     for button in self.sprites():
+    #         button.image = button.norm.copy()
+    #         button.rect = button.norm_rect.copy()
 
-    def display_button(self, screen):
-        for button in self.sprites():
-            screen.blit(button.display_surf, button.display_rect)
+
+# Implement Q&A mechanic with ChatGPT here
+# Utitlize "chatgpt.py"
+
+# class Q_and_A():
+#     def __init__():
+#         pass
+
+# class Question(Q_and_A):
+#     def __init__(self, Ques_text, field_size, field_color, center_pos):
+#         pass
+
--- projects/AIST1110 Group Project/main.py
+++ projects/AIST1110 Group Project/main.py
@@ -1,69 +1,65 @@
 import pygame
-from Resources.background.load_background import load_background
+from load_background import load_background
 from Resources.colors_and_fonts import BREE_SERIF,LIGHT_GRAY, RED
 from Sprites import Button, Button_Group
 from sys import exit
 
 # Some main screens
-# 1. Main Menua Interface
+# 1. Main Menual Interface
 # 2. Game Scene - 
 #       During gameplay (Q&A)
 #       Winning Celebration Animation
 #       Score/Ranking Visualization
-# 3. (more...) 
+# 3. (more...)
 
 
 pygame.init()
-screen0 = pygame.display.set_mode((1280, 720), pygame.SCALED)
 # pygame.RESIZABLE allows window toggling but requires alignment adjustment
 # Decide whether to use pygame.RESIZABLE
+screen0 = pygame.display.set_mode((1280, 720), pygame.SCALED)
 icon = pygame.image.load(r"Resources\icon\game_icon1.png")
 pygame.display.set_icon(icon)
-
-
-
 pygame.display.set_caption("Guessing Their Answers")
 clock = pygame.time.Clock()
 
 # TODO: Look for resources: background.png, characters.png, ...
 title_font = pygame.font.Font(BREE_SERIF, 65)
-title_surf = title_font.render('Guessing Their Answers', True, LIGHT_GRAY, RED)
+title_surf = title_font.render("Guessing Their Answers", True, LIGHT_GRAY, RED)
 title_rect = title_surf.get_rect(midbottom=(640, 160))
 
-play_button = Button("PLAY", button_color=LIGHT_GRAY, button_size=(220, 100), center_pos=(640, 260))
-lang_button = Button("Language", button_color=LIGHT_GRAY, button_size=(220, 100), center_pos= (640, 390))
-diff_button = Button("Difficulty", button_color=LIGHT_GRAY, button_size=(220, 100), center_pos=(640, 520))
+play_button = Button("PLAY", center_pos=(640, 260))
+lang_button = Button("Language", center_pos= (640, 390))
+diff_button = Button("Difficulty", center_pos=(640, 520))
 
 button_group = Button_Group()
 button_group.add(play_button, lang_button, diff_button)
 
 # For background, check Resources (1. sky&clouds 2. ocean&clouds)
-...
+CloudFileBG = {0: "Morning", 1: "Evening", 2: "Night1", 3: "Night2"}
+cloud_filepath = r"Resources\background\Cloud-and-Sky-craftpix-net-995711"
+index = lambda: pygame.time.get_ticks() // 4000 % 4
 
-running = True
-while running:
-    mousePos = pygame.mouse.get_pos()
-    button_group.update(mousePos, pygame.MOUSEMOTION)
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-            exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            button_group.update(mousePos, event.type)
+# Main Game Loop
+if __name__ == "__main__":
+    running = True
+    while running:
+        mousePos = pygame.mouse.get_pos()
+        clicked = False
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                clicked = True
+        
+        # Updating the output window
+        screen = load_background(screen0, cloud_filepath, CloudFileBG[index()])
+        screen.blit(title_surf, title_rect)
+        
+        button_group.update(mousePos, clicked)
+        button_group.draw(screen)
 
-
-    # TODO: Create rectangles to place characters and texts
-    # e.g. question text field, answer text field, ...
-    # OR, draw the required field using pygame.draw
-
-    CloudFileBG = {0: "Morning", 1: "Evening", 2: "Night1", 3: "Night2"}
-    index = pygame.time.get_ticks() // 4000 % 4
-    screen = load_background(screen0, r"Resources\background\Cloud-and-Sky-craftpix-net-995711", CloudFileBG[index])
-    screen.blit(title_surf, title_rect)
-    button_group.display_button(screen)
-    button_group.return_to_normal()
-        # TODO: some redudant codes here
-    pygame.display.flip()
-    clock.tick(30)
+        pygame.display.flip()
+        clock.tick(60)
